fa6f239fa7df9743a7062ac51ef7a5de327def2c
Ric-toledo <152932438+Ric-toledo@users.noreply.github.com>
2024-08-23 20:45:50 -0300

    att5

--- screen-principal.php
+++ screen-principal.php
@@ -4,7 +4,7 @@ session_start();
 if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
     unset($_SESSION['email']);
     unset($_SESSION['senha']);
-    header('Location: ./pages/login.php');
+    header('Location: login.php');
 }
 
 $email = $_SESSION['email'];
@@ -16,9 +16,33 @@ $username = $_SESSION['username'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/screen-principal.css">
+    <title>Estude para o Futuro</title>
 </head>
 <body>
+    <header>
+        <div class="container">
+            <img src="img/Logo.png" alt="Logo"> 
+            <!-- Logo header -->
+            <nav>
+                <ul>
+                    <li>HOME</li>
+                    <li>SOBRE</li>
+                    <li>CURSOS</li>
+                    <li>DÚVIDAS</li>
+                    <li>CONTATO</li>                  
+                </ul>
+            </nav>
+            <!--Nav header -->
+            <div class="dropdown">
+                <a href="login.php"><input type="submit" value="Log in"></a>
+                <a href="cadastro.php"><input type="submit" value="Cadastre-se"></a>
+            </div>
+            <!-- <BUtton>Login</BUtton> -->
+
+            <!-- buttom -->
+        </div>
+    </header>
     <?php
         echo "<h1>Bem vindo <u>$username</u></h1>";
     ?>
